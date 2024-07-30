fc7af647b64f86c3504ab711d784f6503288d7db
Vinicius Chociai Naginski <vinichociai155@gmail.com>
2024-07-30 18:36:45 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Exemplo de JavaScript para filtragem de processos
+//document.querySelectorAll('.btn-outline-secondary').forEach(button => {
+//    button.addEventListener('click', function() {
+//        alert('Filtro aplicado: ' + this.textContent);
+//    });
+//});
